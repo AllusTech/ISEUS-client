32593429f22dc18615d8589811c64a50aaa61050
AgentBraun <AgentBraun@users.noreply.github.com>
2023-10-09 01:59:22 +0200

    test za jiru 3

--- src/pages/Professor/Professors/Professors.tsx
+++ src/pages/Professor/Professors/Professors.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 type Props = {};
 
 const Professors = (props: Props) => {
-  return <div>Professors test za jiru 2</div>;
+  return <div>Professors test za jiru 3</div>;
 };
 
 export default Professors;
